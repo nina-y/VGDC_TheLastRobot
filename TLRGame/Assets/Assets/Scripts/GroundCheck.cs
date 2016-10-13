135119ec2ccaf51672d0813580f32c0fa20c9cde
Brian Tom <btom.0831@gmail.com>
2016-10-13 10:12:24 -0700

    Add Unity and update gitignore

---
+++ TLRGame/Assets/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,31 @@
+﻿/* GroundCheck
+ * Script for setting value of isGrounded using box collider
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour
+{
+	private Player player;
+
+	void Start ()
+	{
+		player = gameObject.GetComponentInParent<Player> ();
+	}
+	// true when player is on ground
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		player.isGrounded = true;
+	}
+
+	void OnTriggerStay2D (Collider2D col)
+	{
+		player.isGrounded = true;
+	}
+	// false when player is off ground
+	void OnTriggerExit2D (Collider2D col)
+	{
+		player.isGrounded = false;
+	}
+}
