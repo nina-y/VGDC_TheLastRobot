ee397ab0e5f33d6c63683bba780f8bea912449ea
Brian Tom <btom.0831@gmail.com>
2016-10-13 10:31:38 -0700

    Add TLRgame

---
+++ TLRgame/Assets/Assets/Scripts/HUD.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+	public Sprite[] heartSprites;
+	public Image healthUI;
+
+	public Player player;
+
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
+	}
+
+	void Update ()
+	{
+		healthUI.sprite = heartSprites [player.currHealth];
+	}
+}
